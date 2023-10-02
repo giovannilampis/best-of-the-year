95c557db912c82d0d0bd5048e410addee4a8d8b2
giovannilampis <giolampis@hotmail.com>
2023-10-02 17:59:58 +0200

    fix Milestone 1

--- src/main/java/org/java/app/controller/NameController.java
+++ src/main/java/org/java/app/controller/NameController.java
@@ -1,8 +1,13 @@
 package org.java.app.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class NameController {
@@ -10,13 +15,37 @@ public class NameController {
 	@GetMapping("/")
 	@ResponseBody
 	public String name() {
-		
-		return "Best of the year by ...";
+		final String myName = "GIOVANNI";
+		return "Best of the year by ..." + myName;
 	}
 	
 	@GetMapping("/best")
-	public String best() {
+	public String best(Model model) {
+		final String name = "GIOVANNI";
+		
+		model.addAttribute("name", name);
 		
 		return "best";
 	}
+	
+	@GetMapping("/movies")
+	public String getMovies(Model model) {
+		
+		List<Movie> movies = getBestMovies();
+		
+		model.addAttribute("movies", movies);
+		
+		return "movies";
+	}
+	
+	@GetMapping("/songs")
+	public String getSongs(Model model) {
+		
+		List<Song> songs = getBestSongs();
+		
+		((Object) model).addAttribute("songs", songs);
+		
+		return "songs";
+	}
+		
 }
