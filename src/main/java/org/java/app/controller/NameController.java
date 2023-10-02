1b68baf33e672723eafab3672b4573895ab2d44a
giovannilampis <giolampis@hotmail.com>
2023-10-02 16:23:20 +0200

    add NameController & best.html

---
+++ src/main/java/org/java/app/controller/NameController.java
@@ -0,0 +1,16 @@
+package org.java.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class NameController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String name() {
+		
+		return "Best of the year by ...";
+	}
+}
